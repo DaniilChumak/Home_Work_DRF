9d5718411f518924e37bf687a79d9214051f301d
user <danilchumak80@gmail.com>
2024-09-03 12:39:00 +0300

    Применили black

--- users/models.py
+++ users/models.py
@@ -1,34 +1,33 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     username = None
     email = models.EmailField(
-        unique=True,
-        verbose_name='Почта',
-        help_text='Введите вашу электронную почту'
+        unique=True, verbose_name="Почта", help_text="Введите вашу электронную почту"
     )
     phone = models.CharField(
         max_length=20,
         blank=True,
         null=True,
-        verbose_name='Телефон',
-        help_text='Введите ваш номер телефона'
+        verbose_name="Телефон",
+        help_text="Введите ваш номер телефона",
     )
     city = models.CharField(
         max_length=35,
         blank=True,
         null=True,
-        verbose_name='Город',
-        help_text='Введите ваш город'
+        verbose_name="Город",
+        help_text="Введите ваш город",
     )
     avatar = models.ImageField(
-        upload_to='users/avatars/',
+        upload_to="users/avatars/",
         blank=True,
         null=True,
-        verbose_name='Аватар',
-        help_text='Загрузите ваш аватар'
+        verbose_name="Аватар",
+        help_text="Загрузите ваш аватар",
     )
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
