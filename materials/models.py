48e3cdac6001af92999f4b898e7672b564b8c9cb
user <danilchumak80@gmail.com>
2024-09-03 14:46:09 +0300

    подредактировали модель

--- materials/models.py
+++ materials/models.py
@@ -36,7 +36,9 @@ class Lesson(models.Model):
     )
     course = models.ForeignKey(
         Course,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
         verbose_name="Курс",
         help_text="Выберите курс",
     )
