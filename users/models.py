f8401d750e68a0f9fc1a4d9633e299f9bd80d695
user <danilchumak80@gmail.com>
2024-09-03 12:37:35 +0300

    Создали модель для пользователя, добавили авторизацию по email

--- users/models.py
+++ users/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True,
+        verbose_name='Почта',
+        help_text='Введите вашу электронную почту'
+    )
+    phone = models.CharField(
+        max_length=20,
+        blank=True,
+        null=True,
+        verbose_name='Телефон',
+        help_text='Введите ваш номер телефона'
+    )
+    city = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True,
+        verbose_name='Город',
+        help_text='Введите ваш город'
+    )
+    avatar = models.ImageField(
+        upload_to='users/avatars/',
+        blank=True,
+        null=True,
+        verbose_name='Аватар',
+        help_text='Загрузите ваш аватар'
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
