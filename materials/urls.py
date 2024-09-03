6087c32dee931a53e730255576c1ec0ba088677a
user <danilchumak80@gmail.com>
2024-09-03 14:47:30 +0300

    добавили пути для дженериков уроков

--- materials/urls.py
+++ materials/urls.py
@@ -1,15 +1,22 @@
-from  rest_framework.routers import SimpleRouter
-from materials.views import CourseViewSet
-from materials.apps import MaterialsConfig
+from django.urls import path
+from rest_framework.routers import SimpleRouter
 
+from materials.apps import MaterialsConfig
+from materials.views import (CourseViewSet, LessonCreateApiView,
+                             LessonDestroyApiView, LessonListApiView,
+                             LessonRetrieveApiView, LessonUpdateApiView)
 
 app_name = MaterialsConfig.name
 
 router = SimpleRouter()
-router.register('', CourseViewSet, basename='course'  )
+router.register("", CourseViewSet, basename="course")
 
 urlpatterns = [
-
+    path("lessons/", LessonListApiView.as_view(), name="lesson-list"),
+    path("lessons/<int:pk>/", LessonRetrieveApiView.as_view(), name="lesson-retrieve"),
+    path("lessons/create/", LessonCreateApiView.as_view(), name="lesson-create"),
+    path("lessons/<int:pk>/delete/", LessonDestroyApiView.as_view(), name="lesson-delete"),
+    path("lessons/<int:pk>/update/", LessonUpdateApiView.as_view(), name="lesson-update"),
 ]
 
 urlpatterns += router.urls
