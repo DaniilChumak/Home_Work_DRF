11a3f2c8b53317c2f6af0501f455f79e7313fdc0
user <danilchumak80@gmail.com>
2024-09-03 12:58:08 +0300

    Создали модель для курсов с уроками

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name="Название курса",
+        help_text="Введите название курса",
+    )
+    preview_image = models.ImageField(
+        upload_to="materials/courses/previews/",
+        blank=True,
+        null=True,
+        verbose_name="Картинка",
+        help_text="Загрузите картинку для превью курса",
+    )
+    description = models.TextField(
+        verbose_name="Описание курса",
+        help_text="Введите описание курса",
+    )
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name="Название урока",
+        help_text="Введите название урока",
+    )
+    description = models.TextField(
+        verbose_name="Описание урока",
+        help_text="Введите описание урока",
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        verbose_name="Курс",
+        help_text="Выберите курс",
+    )
+    preview_image = models.ImageField(
+        upload_to="materials/lessons/previews/",
+        blank=True,
+        null=True,
+        verbose_name="Картинка",
+        help_text="Загрузите картинку для превью курса",
+    )
+    link_to_video = models.CharField(
+        max_length=150,
+        blank=True,
+        null=True,
+        verbose_name="Ссылка на видео",
+        help_text="Укажите ссылку на видео",
+    )
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
