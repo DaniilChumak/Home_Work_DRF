a5be4e172bac6494a6c28e0a694355348511ac8f
user <danilchumak80@gmail.com>
2024-09-06 17:14:29 +0300

    Добавили модель для Платежей

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from materials.models import Course, Lesson
 
 
 class User(AbstractUser):
@@ -35,3 +36,44 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+"""Добавили модель платежа"""
+class Payment(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="пользователь",
+        help_text="Выберите пользователя"
+    )
+    date_of_payment = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата оплаты",
+        blank=True,
+        null=True
+    )
+    course_paid = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        verbose_name="Приобретенный курс",
+        help_text="Выберите курс, который покупаете"
+    )
+    lesson_paid = models.ForeignKey(
+        Lesson,
+        on_delete=models.CASCADE,
+        verbose_name="Приобретенный урок",
+        help_text="Выберите урок, который покупаете"
+    )
+    payment_method = models.CharField(
+        max_length=35,
+        verbose_name="Способ оплаты",
+        help_text="Выберите способ оплаты",
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return f'{self.user}: {self.date_of_payment}, {self.course_paid if self.course_paid else self.lesson_paid}'
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
