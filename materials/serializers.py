b3f2eb6010fb3353d8ab112cbd15ec6c01ff5574
user <danilchumak80@gmail.com>
2024-09-06 17:15:24 +0300

    Добавили сериалайзер для вывода количества уроков и информации по урокам

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,12 +1,23 @@
 from rest_framework.serializers import ModelSerializer
-
+from rest_framework import serializers
 from materials.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    count_of_lessons = serializers.SerializerMethodField()
+    lessons_info = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_number_of_lessons(obj):
+        return obj.count_of_lessons
+
+    def get_lessons_info(obj):
+        return LessonSerializer(obj.lessons_info.all(), many=True).data
+
     class Meta:
         model = Course
-        fields = "__all__"
+        """Выводим все поля, добавляя поле количества уроков"""
+        fields = ("title", "description", "preview_image", "count_of_lessons")
 
 
 class LessonSerializer(ModelSerializer):
