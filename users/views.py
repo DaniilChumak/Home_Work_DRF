2b600ebb5df953f609bbf5b83ac936efa848d581
user <danilchumak80@gmail.com>
2024-09-06 17:17:06 +0300

    Настроили фильтрацию для эндпоинта вывода списка платежей

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.filters import SearchFilter, OrderingFilter
 
-# Create your views here.
+from users.models import Payment
+from users.serializers import PaymentSerializer
+from rest_framework.generics import ListAPIView
+
+
+class PaymentListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    """Настроили фильтрацию"""
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['paid_lesson', 'paid_course', 'payment_method']
+    ordering_fields = ['-date_of_payment']
