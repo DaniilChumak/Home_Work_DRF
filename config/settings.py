c514e5acd52e6397c9cbc8de0258dbc685b6392d
user <danilchumak80@gmail.com>
2024-09-12 18:23:12 +0300

    Добавили библиотеку авторизации JWT

--- config/settings.py
+++ config/settings.py
@@ -110,7 +110,20 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
 
 """Настройка для фильтрации аттрибутов приложения"""
+"""Настройка параметров авторизации JWT"""
+
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend'),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+            'rest_framework_simplejwt.authentication.JWTAuthentication',
+        ],
+    'DEFAULT_PERMISSION_CLASSES': [
+            'rest_framework.permissions.IsAuthenticated',
+        ]
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
