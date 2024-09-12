1724b344fb0ff83bc163ee9ad111f46c800b9111
user <danilchumak80@gmail.com>
2024-09-12 18:28:57 +0300

    Создали urls.py, с указанием вьюсетов

---
+++ users/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from rest_framework.permissions import AllowAny
+from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from users.apps import UsersConfig
+from users.views import UserViewSet, UserCreateAPIView, PaymentListAPIView, UserRetrieveAPIView, UserUpdateAPIView, \
+    UserDeleteAPIView, UserListAPIView
+
+app_name = UsersConfig.name
+router = SimpleRouter()
+router.register('', UserViewSet)
+
+urlpatterns = [
+    path('payment/', PaymentListAPIView.as_view(), name='payment_list'),
+    path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('detail/<int:pk>/', UserRetrieveAPIView.as_view(), name='user_detail'),
+    path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
+    path('delete/<int:pk>/', UserDeleteAPIView.as_view(), name='user_delete'),
+    path('users_list/', UserListAPIView.as_view(), name='users_list'),
+    path('token/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='token'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
+]
+urlpatterns += router.urls
